41983cbb6b138f3bc1271a36f6be458f1a62b6ba
zhouwuquan <173484695@qq.com>
2019-07-14 16:35:53 +0800

    jenkins

---
+++ src/main/java/com/zhou/shi/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.zhou.shi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping("/test")
+	public String test() {
+		System.out.println("2222");
+		return "1111";
+	}
+}
